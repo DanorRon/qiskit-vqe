4bad0aa36f6a6c9cd630a4c1bdb8790a64568302
Ronan Venkat <47954337+DanorRon@users.noreply.github.com>
2022-10-24 14:16:20 -0400

    Add unused max_evals_grouped code

--- 1d TFIM/Real Quantum Computer/vqe_run.py
+++ 1d TFIM/Real Quantum Computer/vqe_run.py
@@ -3,14 +3,14 @@ from qiskit import Aer, IBMQ, QuantumCircuit
 import vqe_functions
 from vqe_functions import VQE
 
-#backend = Aer.get_backend('aer_simulator')
+backend = Aer.get_backend('aer_simulator')
 
 #IBMQ.save_account('fb3e08528d7c6638591c88e8a957996475aea67811f7dee0916862a946430748a2791e32a01f89e7e3d7fe7030855562e3bafa9014591b9ea50eeadb220858f3')
-provider = IBMQ.load_account()
+#provider = IBMQ.load_account()
 #print(IBMQ.providers())
 #provider = IBMQ.get_provider(hub='ibm-q', group='open', project='main')
 #print(provider.backends())
-backend = provider.get_backend('ibmq_manila')
+#backend = provider.get_backend('ibmq_qasm_simulator')
 
 
 num_qubits = 3
@@ -19,7 +19,7 @@ shots = 10000
 h_zz = -1
 h_z = 1
 h_x = 1
-maxiter = 300
+maxiter = 100
 
 vqe = VQE(num_qubits, layers, shots, h_zz, h_z, h_x, maxiter, backend)
 initial_thetas = vqe.initial_params()
